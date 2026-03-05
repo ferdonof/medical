c8c6542b59dcd88492541382e1f87c7bb7349f67
ferdonof <pmfernan@gmail.com>
2026-03-05 09:05:33 +0100

    feat: create patients table

---
+++ appointments-management/appointments-management-infrastructure/appointments-management-infrastructure-repository-psql/src/db/liquibase/scripts/0.0.0/ddl/000-create-patients-table.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+--changeset pmfernan@gmail.com:medical_appointments_0.0.0_0
+--comment: CREATE patient table
+
+CREATE TABLE patients (
+           id UUID PRIMARY KEY,
+           name VARCHAR(10) NOT NULL,
+           email VARCHAR(150) NOT NULL,
+           phone VARCHAR(50) NOT NULL,
+           created_at TIMESTAMPTZ(6) NOT NULL,
+           updated_at TIMESTAMPTZ(6) NOT NULL
+);
+
+CREATE INDEX idx_patients_email ON patients (email);
+
+--rollback DROP TABLE patient;
+--rollback DROP INDEX idx_patients_email;
