5783f05991b341e37aa8e1f1e27a8087c6cdf8fb
ferdonof <pmfernan@gmail.com>
2026-03-05 18:45:28 +0100

    feat: update controller

--- appointments-management/appointments-management-rest-endpoints/appointments-management-rest-endpoints-handle-appointments/src/main/java/com/ferdonof/medical/config/AppointmentsControllerConfig.java
+++ appointments-management/appointments-management-rest-endpoints/appointments-management-rest-endpoints-handle-appointments/src/main/java/com/ferdonof/medical/config/AppointmentsControllerConfig.java
@@ -1,4 +1,17 @@
 package com.ferdonof.medical.config;
 
+import com.ferdonof.medical.appointments.ports.AppointmentsProcessManager;
+import com.ferdonof.medical.appointments.services.AppointmentReservationService;
+import com.ferdonof.medical.appointments.services.impl.AppointmentReservationServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppointmentsControllerConfig {
+
+  @Bean
+  public AppointmentReservationService appointmentReservationService(AppointmentsProcessManager processManager) {
+    return new AppointmentReservationServiceImpl(processManager);
+  }
+
 }
--- appointments-management/appointments-management-rest-endpoints/appointments-management-rest-endpoints-handle-appointments/src/main/java/com/ferdonof/medical/controllers/AppointmentsController.java
+++ appointments-management/appointments-management-rest-endpoints/appointments-management-rest-endpoints-handle-appointments/src/main/java/com/ferdonof/medical/controllers/AppointmentsController.java
@@ -1,7 +1,15 @@
 package com.ferdonof.medical.controllers;
 
+import com.ferdonof.medical.appointments.services.AppointmentReservationService;
+import com.ferdonof.medical.appointments.services.entities.AppointmentReservationRequest;
+import com.ferdonof.medical.dtos.AppointmentReservationRequestDTO;
+import com.ferdonof.medical.dtos.AppointmentReservationResponseDTO;
+import com.ferdonof.medical.mappers.AppointmentReservationMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AppointmentsController {
 
+	private final AppointmentReservationMapper reservationMapper;
+	private final AppointmentReservationService reservationService;
+
 	@GetMapping
-	public String getAppointments() {
-		return "Get Appointments";
+	public String get() {
+		return "Get Appointments is working!";
+	}
+
+	@PostMapping
+	public AppointmentReservationResponseDTO createAppointment(
+			@Valid @RequestBody AppointmentReservationRequestDTO request) {
+		final AppointmentReservationRequest domainRequest = this.reservationMapper.map(request);
+		return this.reservationMapper.map(this.reservationService.execute(domainRequest));
 	}
 
 }
