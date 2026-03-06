e1be3666213f8ab89b0506412658ba25c622fb23
ferdonof <pmfernan@gmail.com>
2026-03-06 13:34:53 +0100

    feat: update appointment process manager adapter

--- appointments-management/appointments-management-infrastructure/appointments-management-infrastructure-bpmn/appointment-process/src/main/java/com/ferdonof/medical/adapters/AppointmentProcessManagerAdapter.java
+++ appointments-management/appointments-management-infrastructure/appointments-management-infrastructure-bpmn/appointment-process/src/main/java/com/ferdonof/medical/adapters/AppointmentProcessManagerAdapter.java
@@ -1,14 +1,18 @@
 package com.ferdonof.medical.adapters;
 
+import java.util.Map;
+
 import com.ferdonof.medical.appointments.entities.PatientSymptoms;
 import com.ferdonof.medical.appointments.ports.AppointmentsProcessManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.history.HistoricProcessInstance;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AppointmentProcessManagerAdapter implements AppointmentsProcessManager {
@@ -23,14 +27,19 @@ public class AppointmentProcessManagerAdapter implements AppointmentsProcessMana
 
 	private final RuntimeService runtimeService;
 
+	private final HistoryService historyService;
+
 	@Override
 	public void startAppointmentProcess(PatientSymptoms symptoms) {
 		final ProcessInstance appointmentProcess = this.runtimeService.startProcessInstanceByKey("appointment_process",
 				this.toProcessVariables(symptoms));
 
-		final String processInstanceId = appointmentProcess.getId();
-		System.out.println("Started appointment process with ID: " + processInstanceId);
-		System.out.println("Process variables: " + this.runtimeService.getVariables(processInstanceId));
+		final HistoricProcessInstance historic = this.historyService.createHistoricProcessInstanceQuery()
+				.processInstanceId(appointmentProcess.getProcessInstanceId()).singleResult();
+
+		final String processInstanceId = historic.getId();
+
+		log.info("Appointment process with ID: {} and status: {}", processInstanceId, historic.getState());
 
 	}
 
