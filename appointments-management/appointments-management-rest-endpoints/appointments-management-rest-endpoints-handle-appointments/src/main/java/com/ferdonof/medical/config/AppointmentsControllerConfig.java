96d38db16f78c32b0e619313ed27a41cb1574d52
ferdonof <pmfernan@gmail.com>
2026-03-08 15:40:03 +0100

    feat: update controller and config

--- appointments-management/appointments-management-rest-endpoints/appointments-management-rest-endpoints-handle-appointments/src/main/java/com/ferdonof/medical/config/AppointmentsControllerConfig.java
+++ appointments-management/appointments-management-rest-endpoints/appointments-management-rest-endpoints-handle-appointments/src/main/java/com/ferdonof/medical/config/AppointmentsControllerConfig.java
@@ -1,17 +1,35 @@
 package com.ferdonof.medical.config;
 
-import com.ferdonof.medical.appointments.ports.AppointmentsProcessManager;
-import com.ferdonof.medical.appointments.services.AppointmentReservationService;
-import com.ferdonof.medical.appointments.services.impl.AppointmentReservationServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.ferdonof.medical.appointments.ports.AppointmentAttendPatientPort;
+import com.ferdonof.medical.appointments.ports.AppointmentConfirmationHandlerPort;
+import com.ferdonof.medical.appointments.ports.AppointmentsProcessStarterPort;
+import com.ferdonof.medical.appointments.services.AppointmentAttendPatientService;
+import com.ferdonof.medical.appointments.services.AppointmentAttendPatientServiceImpl;
+import com.ferdonof.medical.appointments.services.AppointmentConfirmationService;
+import com.ferdonof.medical.appointments.services.AppointmentConfirmationServiceImpl;
+import com.ferdonof.medical.appointments.services.AppointmentReservationService;
+import com.ferdonof.medical.appointments.services.AppointmentReservationServiceImpl;
 
 @Configuration
 public class AppointmentsControllerConfig {
 
   @Bean
-  public AppointmentReservationService appointmentReservationService(AppointmentsProcessManager processManager) {
+	public AppointmentReservationService appointmentReservationService(AppointmentsProcessStarterPort processManager) {
     return new AppointmentReservationServiceImpl(processManager);
   }
 
+	@Bean
+	public AppointmentConfirmationService appointmentConfirmationService(
+			AppointmentConfirmationHandlerPort appointmentConfirmationHandlerPort) {
+		return new AppointmentConfirmationServiceImpl(appointmentConfirmationHandlerPort);
+	}
+
+	@Bean
+	public AppointmentAttendPatientService patientSeenService(
+			AppointmentAttendPatientPort appointmentMarkPatientSeenPort) {
+		return new AppointmentAttendPatientServiceImpl(appointmentMarkPatientSeenPort);
+	}
 }
--- appointments-management/appointments-management-rest-endpoints/appointments-management-rest-endpoints-handle-appointments/src/main/java/com/ferdonof/medical/controllers/AppointmentsController.java
+++ appointments-management/appointments-management-rest-endpoints/appointments-management-rest-endpoints-handle-appointments/src/main/java/com/ferdonof/medical/controllers/AppointmentsController.java
@@ -1,17 +1,24 @@
 package com.ferdonof.medical.controllers;
 
-import com.ferdonof.medical.appointments.services.AppointmentReservationService;
-import com.ferdonof.medical.appointments.services.entities.AppointmentReservationRequest;
-import com.ferdonof.medical.dtos.AppointmentReservationRequestDTO;
-import com.ferdonof.medical.dtos.AppointmentReservationResponseDTO;
-import com.ferdonof.medical.mappers.AppointmentReservationMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ferdonof.medical.appointments.services.AppointmentAttendPatientService;
+import com.ferdonof.medical.appointments.services.AppointmentConfirmationService;
+import com.ferdonof.medical.appointments.services.AppointmentReservationService;
+import com.ferdonof.medical.appointments.services.entities.AppointmentReservationRequest;
+import com.ferdonof.medical.dtos.AppointmentInfoDTO;
+import com.ferdonof.medical.dtos.AppointmentReservationRequestDTO;
+import com.ferdonof.medical.dtos.AppointmentReservationResponseDTO;
+import com.ferdonof.medical.mappers.AppointmentReservationMapper;
 
 @RestController
 @RequestMapping("/appointments")
@@ -20,6 +27,8 @@ public class AppointmentsController {
 
 	private final AppointmentReservationMapper reservationMapper;
 	private final AppointmentReservationService reservationService;
+	private final AppointmentConfirmationService confirmationService;
+	private final AppointmentAttendPatientService attendPatientService;
 
 	@GetMapping
 	public String get() {
@@ -27,10 +36,22 @@ public class AppointmentsController {
 	}
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public AppointmentReservationResponseDTO createAppointment(
 			@Valid @RequestBody AppointmentReservationRequestDTO request) {
 		final AppointmentReservationRequest domainRequest = this.reservationMapper.map(request);
 		return this.reservationMapper.map(this.reservationService.execute(domainRequest));
 	}
 
+	@PostMapping("/confirm")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public void confirm(@Valid @RequestBody AppointmentInfoDTO request) {
+		this.confirmationService.execute(request.id());
+	}
+
+	@PostMapping("/attend")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public void markAsSeen(@Valid @RequestBody AppointmentInfoDTO request) {
+		this.attendPatientService.execute(request.id());
+	}
 }
